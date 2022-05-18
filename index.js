f12e1563ba417630fded99eea54bd2a652c7d0bf
Alextonio-Winatian <101455767+Alextonio-Winatian@users.noreply.github.com>
2022-05-18 13:15:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mysql = require("mysql")
 const app = express();
+const bcrypt = require("bcrypt")
 
 app.use(express.json());
 
@@ -13,6 +14,19 @@ app.get("/", async (req, res)=> {
 	res.json({ status: "Waras APP READY!!"})
 });
 
+// GET ALL DATA
+app.get("/:tbuserwaras", async (req, res)=>{
+	const query = "SELECT * FROM tbuserwaras";
+	pool.query(query, [req.params.tbuserwaras], (error, result)=>{
+		if (!result[0]) {
+			res.json({ status: "Not found!"});
+		} else {
+			req.json(result[0]);
+		} 
+	});
+});
+
+// GET USER DATA BY USERNAME
 app.get("/:tbuserwaras", async (req, res)=>{
 	const query = "SELECT * FROM tbuserwaras WHERE username= ?";
 	pool.query(query, [req.params.tbuserwaras], (error, result)=>{
@@ -31,23 +45,44 @@ const pool = mysql.createPool({
 	socketPath: process.env.INSTANCE_CONNECTION_NAME,
 });
 
+// POST USER DATA TO DB
 app.post("/", async (req, res)=>{
+	const hashedPassword = await bcrypt.hash(req.body.password, 10)
 	const data = {
 		username: req.body.username,
 		full_name: req.body.full_name,
 		email: req.body.email,
-		password: req.body.password,
+		password: hashedPassword,
 		telephone: req.body.telephone,
 		date_of_birth: req.body.date_of_birth,
-		created_at: req.body.created_at,
-		updated_at: req.body.updated_at,
+		created_at: new Date().toISOString(),
+		updated_at: created_at,
 	}
+	//DATABASE
 	const query = "INSERT INTO tbuserwaras VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 	pool.query(query, Object.values(data), (error)=>{
 		if (error){
 			res.json({ status: "failure", reason: error.code });
 		} else {
 			res.json({ status: "success", data: data });
+		}
+	});
+});
+
+app.post('/:tbuserwaras', async (req, res) => {
+  const query = "SELECT * FROM tbuserwaras WHERE username= ?";
+	pool.query(query, [req.params.tbuserwaras], (error, result)=>{
+		if (!result[0]) {
+			res.json({ status: "User not found!", reason: error.code});
+		}	
+		try {
+			if(await bcrypt.compare(req.body.password, query.password)) {
+				res.json({ status: "Success"});
+			} else {
+				res.json({ status: "password or username does't match" });
+			}
+		} catch {
+			res.json({reason: error.code});
 		}
 	});
 });
